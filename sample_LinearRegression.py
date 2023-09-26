bae0a5d42f371806b7faf638f9371c6429e6e047
leeheekyo <glhk5895@naver.com>
2023-09-27 04:42:41 +0900

    update code

--- sample_LinearRegression.py
+++ sample_LinearRegression.py
@@ -6,7 +6,7 @@ x = np.arange(-5,5,0.1)
 n = len(x)
 newX = np.reshape(x,(n,1))
 
-y = np.dot(newX, np.array([1]))
+y = np.dot(newX, np.array([1])) + 1
 yNoise = 1 * np.random.normal(size=n)
 y = y + yNoise
 
@@ -21,6 +21,6 @@ reg = linear_regression.fit(newX, y)
 # print(reg.intercept_)
 
 plt.scatter(x, y)
-plt.plot(newX, reg.coef_*x, c="red")
+plt.plot(newX, reg.coef_*x + 1, c="red")
 
 plt.show()
